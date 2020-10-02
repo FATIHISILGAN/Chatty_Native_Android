9a1d418c1bb6654b8a848cef5f5f388dbfc2ac05
Fatih ışılgan <fatihisilgan@hotmail.com>
2020-10-02 18:08:40 +0300

    Realtime message and chating done,

--- app/src/main/java/com/example/chatty/MainActivity.java
+++ app/src/main/java/com/example/chatty/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                                 list.add(snapshot.getKey());
 
                                 userAdapter.notifyDataSetChanged();
+
                             }
 
 
